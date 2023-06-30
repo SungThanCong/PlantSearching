8fe2358b417147010367d3a58743a122b7e884d6
Chinh <blueskylove2002@gmail.com>
2023-06-30 20:55:22 +0700

    update

--- app/src/main/java/com/example/plantsearching/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/plantsearching/Fragment/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -12,13 +13,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.example.plantsearching.Adapter.PhotographyAdapter;
 import com.example.plantsearching.ArticleActivity;
 import com.example.plantsearching.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HomeFragment extends Fragment {
 
     CardView btn_to_article;
     RecyclerView lsvPhotography;
+    ArrayList<String> photographyData = new ArrayList<String>(Arrays.asList(
+            "https://firebasestorage.googleapis.com/v0/b/plan-app-3f6f5.appspot.com/o/photography%2Fphotography_1.png?alt=media&token=d2ea54f4-21a1-4465-bb52-37c2f16669e4"
+            ,"https://firebasestorage.googleapis.com/v0/b/plan-app-3f6f5.appspot.com/o/photography%2Fphotography_1.png?alt=media&token=d2ea54f4-21a1-4465-bb52-37c2f16669e4"
+            ,"https://firebasestorage.googleapis.com/v0/b/plan-app-3f6f5.appspot.com/o/photography%2Fphotography_1.png?alt=media&token=d2ea54f4-21a1-4465-bb52-37c2f16669e4"));
+    PhotographyAdapter adapter;
     public HomeFragment() {
 
     }
@@ -42,6 +52,10 @@ public class HomeFragment extends Fragment {
 
         btn_to_article = view.findViewById(R.id.btn_to_article);
         lsvPhotography = view.findViewById(R.id.lsvPhotography);
+
+        lsvPhotography.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        adapter = new PhotographyAdapter(getActivity(),photographyData);
+        lsvPhotography.setAdapter(adapter);
 
         addEvents();
 
